1ba1afb04c3adc4c13fc4683d26ff3e0b7b23bb4
oumaymawerfelli <147613644+oumaymawerfelli@users.noreply.github.com>
2024-02-29 11:12:05 +0100

    Update Zoo.java

--- Zoo.java
+++ Zoo.java
@@ -1,23 +1,79 @@
 package tn.esprit.gestionzoo.entitees;
 
 public class Zoo {
-
-     public static final int NUMBER_OF_CAGES = 25;
+    public static final int MAX_AQUATIC_ANIMALS = 10;
+    private AquaticAnimal[] aquaticAnimals;
+    private int nbrAquaticAnimals;
+    public static final int NUMBER_OF_CAGES = 25;
     private Animal[] animals;
     private String name, city;
     private int nbrAnimals;
 
     public Zoo() {
         animals = new Animal[NUMBER_OF_CAGES];
+        aquaticAnimals = new AquaticAnimal[MAX_AQUATIC_ANIMALS];
+    }
+    public AquaticAnimal getAquaticAnimal(int index) {
+        if (index >= 0 && index < nbrAquaticAnimals) {
+            return aquaticAnimals[index];
+        }
+        return null; // Retourne null si l'indice est invalide ou s'il n'y a pas d'animal aquatique à cet indice
     }
 
+    public int getNbrAquaticAnimals() {
+        return nbrAquaticAnimals;
+    }
     public Zoo(String name, String city) {
         this.name = name;
         this.city = city;
         animals = new Animal[NUMBER_OF_CAGES];
+        aquaticAnimals = new AquaticAnimal[MAX_AQUATIC_ANIMALS];    }
+    public void addAquaticAnimal(AquaticAnimal aquatic) {
+        if (nbrAquaticAnimals < MAX_AQUATIC_ANIMALS) {
+            aquaticAnimals[nbrAquaticAnimals++] = aquatic;
+            System.out.println("L'animal aquatique a été ajouté avec succès au zoo.");
+        } else {
+            System.out.println("Le zoo aquatique est plein, impossible d'ajouter plus d'animaux aquatiques.");
+        }
+    }public float maxPenguinSwimmingDepth() {
+    float maxDepth = 0;
+
+    // Parcours de tous les animaux aquatiques dans le zoo
+    for (int i = 0; i < nbrAquaticAnimals; i++) {
+        AquaticAnimal animal = aquaticAnimals[i];
+        // Vérifier si l'animal est un pingouin
+        if (animal instanceof Penguin) {
+            // Cast l'animal en pingouin
+            Penguin penguin = (Penguin) animal;
+            // Comparer la profondeur de nage du pingouin actuel avec la profondeur maximale
+            if (penguin.getSwimmingDepth() > maxDepth) {
+                maxDepth = penguin.getSwimmingDepth();
+            }
+        }
     }
 
-    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+    return maxDepth;
+} public void displayNumberOfAquaticsByType() {
+        int numberOfDolphins = 0;
+        int numberOfPenguins = 0;
+
+        // Parcours de tous les animaux aquatiques dans le zoo
+        for (int i = 0; i < nbrAquaticAnimals; i++) {
+            AquaticAnimal animal = aquaticAnimals[i];
+            // Vérifier le type de l'animal aquatique
+            if (animal instanceof Dolphin) {
+                numberOfDolphins++;
+            } else if (animal instanceof Penguin) {
+                numberOfPenguins++;
+            }
+        }
+
+        // Affichage du nombre de dauphins et de pingouins dans le zoo
+        System.out.println("Nombre de dauphins dans le zoo : " + numberOfDolphins);
+        System.out.println("Nombre de pingouins dans le zoo : " + numberOfPenguins);
+    }
+}
+   /* public static Zoo comparerZoo(Zoo z1, Zoo z2) {
         if (z1.nbrAnimals > z2.nbrAnimals)
             return z1;
         return z2;
@@ -102,5 +158,4 @@ public class Zoo {
     @Override
     public String toString() {
         return "Name: " + name + ", City: " + city + ", N° Cages: " + NUMBER_OF_CAGES + ", N° animals: " + nbrAnimals;
-    }
-}
+    }*/
